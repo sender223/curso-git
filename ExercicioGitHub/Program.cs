ab33815fdcd4560e4d06636f33e38b1addb1a03f
Sender Rodrigues dos Santos <sender.rsantos@gmail.com>
2019-09-30 17:47:23 -0300

    testando o pull do git

--- ExercicioGitHub/Program.cs
+++ ExercicioGitHub/Program.cs
@@ -6,10 +6,13 @@ using System.Text;
 namespace ExercicioGitHub {
     class Program {
         static void Main(string[] args) {
-            
+
+            Console.WriteLine( "------------------------------" );
             Console.WriteLine("Olá Mundo!!");
             Console.WriteLine("Tudo bem com você? ");
             Console.WriteLine("Tenha um ótimo dia");
+
+            Console.WriteLine("------------------------------");
 
         }
     }

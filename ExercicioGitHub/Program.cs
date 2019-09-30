7b64493550c3eed85bf72bbbc6f996b5d1b544bc
Sender Santos <sender.rsantos@gmail.com>
2019-09-30 16:42:37 -0300

    teste

---
+++ ExercicioGitHub/Program.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExercicioGitHub {
+    class Program {
+        static void Main(string[] args) {
+            
+            Console.WriteLine("Olá Mundo!!");
+            Console.WriteLine("Tudo bem com você? ");
+            
+
+            
+        }
+    }
+}
